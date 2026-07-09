bc7058318e1696e2d669a87621a30d03531352e3
rivanshugaur <101885035+rivanshugaur@users.noreply.github.com>
2026-07-10 02:26:01 +0530

    Time: 67 ms (29.46%), Space: 111.2 MB (16.83%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int>seen;
+        int n = nums.size();
+        for(int i = 0;i<n;i++){
+            if(seen.count(nums[i])) return true;
+            seen.insert(nums[i]);
+        }
+        return false;
+        
+    }
+};
