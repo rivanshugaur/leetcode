bf87cb50ebfc2380305e82959fb2f1207f58b5b3
rivanshugaur <101885035+rivanshugaur@users.noreply.github.com>
2026-07-14 20:45:07 +0530

    Time: 39 ms (41.5%), Space: 69.2 MB (56.17%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,15 +1,50 @@
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int,vector<int>,greater<int>>pq;//by default it is max head
-        //to make it min heap use greater<int>
+    int partition(vector<int>& nums,int left , int right){
+        if(left>right) return -1;
+        int pivot = nums[left];
+        int i = left+1;
+        int j = right;
 
-        for(auto ele: nums){
-            pq.push(ele);
-            if(pq.size()>k) pq.pop();
+        while(i<=j){
+            if(nums[i]<pivot && nums[j]>pivot){
+                swap(nums[i],nums[j]);
+                i++;
+                j--;
+            }
+            if(nums[i]>=pivot){
+                i++;
+            }
+            if(nums[j]<=pivot){
+                j--;
+            }
+            
         }
-        return pq.top();
+        swap(nums[left],nums[j]);
+        return  j;// pivot is at the jth index
+    }
+
+
+    int findKthLargest(vector<int>& nums, int k) {
+
+        int l = 0;
+        int r = nums.size()-1;
+        int pivot_idx = 0;
+        while(true){
+            pivot_idx=partition(nums,l,r);
+            if(pivot_idx==k-1){
+                break;
+            }
+            else if(pivot_idx>k-1){
+                r = pivot_idx-1;
+            }
+            else{
+                l = pivot_idx+1;
+            }
+            
+        }
+        return nums[pivot_idx];
+
         
     }
 };
-//time complexity nlog(k)
