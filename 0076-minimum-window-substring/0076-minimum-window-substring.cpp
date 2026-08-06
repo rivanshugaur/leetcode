8ae19bf734f9501903f0ccaab4ead33196aa35de
rivanshugaur <101885035+rivanshugaur@users.noreply.github.com>
2026-08-06 16:51:37 +0530

    Time: 3 ms (78.09%), Space: 11.3 MB (94.64%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,43 +1,52 @@
 class Solution {
 public:
+    //commented approach
     string minWindow(string s, string t) {
-        unordered_map<char,int>mp;
-        for(int i = 0;i<t.size();i++){
-            mp[t[i]]++;
-        }
-        int n = s.size();
-        int i = 0;
-        int j = 0;
-        int min_i = 0;
-        int min_j = n-1;
-        int req_count = t.size();
-        bool flag = false;
-        while(i<n && j<n){
-            if(mp[s[j]]>0){
-                req_count--;
-            }
-            mp[s[j]]--;
-            while(req_count==0){
-                flag = true;
-                if((j-i) < (min_j - min_i)){
-                    min_j = j;
-                    min_i = i;
-                }
-                if(mp[s[i]]==0) req_count++;
-
-                mp[s[i]]++;
-                i++;
-            }
-
-            
-            j++;
-        }
-        string ans = "";
-        if(flag==false) return ans;
-        for(int temp = min_i;temp<=min_j;temp++){
-            ans+=s[temp];
-        }
-        return ans;
-        
+    int n = s.size();
+    int m = t.size();
+    if (m == 0 || n < m) return "";
+    
+    // Hash map to store the frequency of characters in string 't'
+    int hash[58] = {0}; 
+    for (int i = 0; i < m; i++) {
+        hash[t[i]-65]++;
     }
+    
+    int l = 0, r = 0; 
+    int minLen = 1e9, sIndex = -1; // sIndex stores the starting index of the min window
+    int cnt = 0; // Number of characters matched
+    
+    while (r < n) {
+        // If the current character is required, increment the valid count
+        if (hash[s[r]-65] > 0) {
+            cnt++;
+        }
+        // Decrease the required frequency for the current character in the map
+        hash[s[r]-65]--;
+        
+        // When all characters of 't' are matched, try to shrink the window
+        while (cnt == m) {
+            // Update the minimum window size and its starting index
+            if (r - l + 1 < minLen) {
+                minLen = r - l + 1;
+                sIndex = l;
+            }
+            
+            // Shrink from the left
+            hash[s[l]-65]++; // Add the character back to the map's required count
+            
+            // If we remove a character that was necessary to form 't', reduce our valid match count
+            if (hash[s[l]-65] > 0) {
+                cnt--;
+            }
+            
+            l++; // Increment left pointer
+        }
+        
+        r++; // Expand the window to the right
+    }
+    
+    // Return the minimum substring, or an empty string if no valid window is found
+    return sIndex == -1 ? "" : s.substr(sIndex, minLen);
+}
 };
